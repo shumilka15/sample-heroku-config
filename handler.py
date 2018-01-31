13db5f369620771f46c375272b3ef705312313b6
shumilka15 <shumilov.george98@gmail.com>
2018-01-31 16:21:46 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,10 +1,10 @@
-smiles = {'smile_name_1':"smile_1", 'smile_name_2':"smile_2", 'smile_name_3':"smile_3", 'smile_name_4':"smile_4", 'smile_name_5':"smile_5"}
-
+smiles = {'Angry':"┌( ◕ 益 ◕ )ᓄ", 'Ugly':"(   ͡°╭╮ʖ   ͡°)", 'Upset':"( ຈ ﹏ ຈ )", 'Cool':"⊂(▀¯▀⊂)", 'Meh':"(╭ರ_⊙)"}
 
 def handle_message(dir, name):
     if name in smiles:
+        return dir[name]
+    else:
         return "Smile is not found"
-    return dir[name]
 
 
 if  __name__ == "__main__":
