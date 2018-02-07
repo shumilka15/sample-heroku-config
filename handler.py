74e1b069f49c9e2709257256fd3ffd3068e881ba
shumilka15 <shumilov.george98@gmail.com>
2018-02-07 16:10:30 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,15 +1,18 @@
-smiles = {'Angry':"┌( ◕ 益 ◕ )ᓄ", 'Ugly':"(   ͡°╭╮ʖ   ͡°)", 'Upset':"( ຈ ﹏ ຈ )", 'Cool':"⊂(▀¯▀⊂)", 'Meh':"(╭ರ_⊙)"}
+import random
+from smiles import smiles
 
 def handle_message(dir, name):
     if name in smiles:
-        return dir[name]
+        return random.choice(dir[name])
+    elif name == "Show me Stats":
     else:
         return "Smile is not found"
 
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     print("Smile list: " + str(smiles.keys()))
     while True:
-        smile_name = input("Enter necessary smile name: ")
+        smile_name = input("Your Message: ")
         ans = handle_message(smiles, smile_name)
         print(ans)
+
