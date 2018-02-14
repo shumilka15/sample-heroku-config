e52ad39a864d1c6864fcab1f2a7661b8d7138062
shumilka15 <shumilov.george98@gmail.com>
2018-02-14 17:14:31 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,10 +1,15 @@
 import random
 from smiles import smiles
+from smiles import stats
 
 
-def handle_message(dir, name):
-    if name in smiles:
-        return random.choice(dir[name])
+def handle_message(name):
+    if name == "Stats":
+        stats_p = [(i, stats[i]) for i in stats]
+        return sorted(stats_p, key=lambda x: -x[1])
+    elif name in smiles:
+        stats[name] += 1
+        return random.choice(smiles[name])
     else:
         return "Smile is not found"
 
@@ -12,7 +17,6 @@ def handle_message(dir, name):
 if __name__ == "__main__":
     print("Smile list: " + str(smiles.keys()))
     while True:
-        smile_name = input("Your Message: ")
-        ans = handle_message(smiles, smile_name)
+        smile_name = input("Type 'Stats' if you want to see statistics.\nIf you want smiles type emotion!\nYour Message:")
+        ans = handle_message(smile_name)
         print(ans)
-
