314e1b9bd7d748dea74abeca8af95ad462d29780
shumilka15 <shumilov.george98@gmail.com>
2018-02-15 18:20:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ import requests
 from handler import handle_message
 
 while True:
-    res = requests.get("https://api.telegram.org/bot502815445:AAEuKSBpwXge3YjTRS8owspX6FvYQZs5lU/getUpdates")
+    res = requests.get("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/getUpdates")
     d = res.json()
     for elem in d["result"]:
         text = elem["message"]
         print(elem["from"]["username"], text)
         ans = handle_message(text)
         chat_id = elem["message"]["chat"]["id"]
-        requests.post("https://api.telegram.org/bot502815445:AAEuKSBpwXge3YjTRS8owspX6FvYQZs5lU/sendMessage", params={"chat_id": chat_id, "text": text} )
+        requests.post("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/sendMessage", params={"chat_id": chat_id, "text": text} )
