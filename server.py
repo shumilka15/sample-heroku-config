8c276c0e118b44902653e4455e171fc0aa694f8d
shumilka15 <shumilov.george98@gmail.com>
2018-03-07 17:40:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ while True:
     d = res.json()
     for elem in d["result"]:
         text = elem["message"]
-        print(elem["from"]["username"], text)
+        print(elem)
+        print(elem["message"]["from"]["username"], text)
         ans = handle_message(text)
         chat_id = elem["message"]["chat"]["id"]
         requests.post("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/sendMessage", params={"chat_id": chat_id, "text": text} )
