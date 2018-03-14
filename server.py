c0be77abc5795d64d89ca233f7cddd70336aab50
shumilka15 <shumilov.george98@gmail.com>
2018-03-14 16:17:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
-from time import sleep
+import requests 
+from handler import handle_message 
+lui = 0 
+while True: 
+    res = requests.get("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/getUpdates", params = {"offset": lui}) 
+    d = res.json() 
 
-import requests
-from handler import handle_message
+    for elem in d["result"]: 
+        text = elem["message"]["text"] 
+        ans = handle_message(text) 
+        lui = elem["update_id"]+1 
+        chat_id = elem["message"]["chat"]["id"] 
 
-while True:
-    res = requests.get("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/getUpdates")
-    d = res.json()
-    for elem in d["result"]:
-        text = elem["message"]
-        print(elem)
-        #print(elem["message"]["from"]["username"], text)
-        ans = handle_message(text)
-        chat_id = elem["message"]["chat"]["id"]
-        requests.post("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/sendMessage", params={"chat_id": chat_id, "text": text} )
+        requests.post("https://api.telegram.org/bot502815445:AAGOuCHPn4pEgdVQtxnuoTMCeWpw6hn27lo/sendMessage", params={ "chat_id": chat_id, "text": ans} )
